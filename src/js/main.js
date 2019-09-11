c63f912aac1dd1db8df732034b9fc66cfede7e77
vadimbelinovich <vadimbelinovich@gmail.com>
2019-09-11 18:14:27 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function() {
     const warning = document.querySelector('section.warning');
     const warningCheck = warning.querySelector('.check input');
+    const link = document.querySelector('nav#navigation .link');
+    const list = document.querySelector('nav#navigation ul.list');
     warningCheck.addEventListener('click', function() {
         warning.style.display = 'none';
         localStorage.setItem('display', 'none');
@@ -10,4 +12,17 @@ window.addEventListener('load', function() {
         const display = localStorage.getItem('display')
         warning.style.display = display;
     }
+
+    link.addEventListener('mouseover', function(e){
+        link.classList.add('first__link');
+    });
+    list.addEventListener('mouseover', function(e){
+        link.classList.add('first__link');
+    });
+    list.addEventListener('mouseleave', function(e){
+        link.classList.remove('first__link');
+    });
+    link.addEventListener('mouseleave', function(e){
+        link.classList.remove('first__link');
+    });
 })
